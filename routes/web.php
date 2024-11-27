59645d12be870f113a88823b2ed0f43a0543cde4
tjanuki <t.januki@gmail.com>
2024-11-27 16:42:10 -0500

    add PHP8.4 omit parentheses example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/php-83', function () {
+    return (new Response())
+        ->header('Content-Type', 'application/json')
+        ->setContent(['status' => 'success'])
+        ->setStatusCode(200);
+});
+
+Route::get('/php-84', function () {
+    return new Response()
+        ->header('Content-Type', 'application/json')
+        ->setContent(['status' => 'success'])
+        ->setStatusCode(200);
 });
